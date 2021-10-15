0bd91a9a903832f310eea91e68fdbc72472e94b5
trvas <trvas@iscte-iul.pt>
2021-10-15 20:07:50 +0100

    same as prev

--- src/main/java/TrelloManager.java
+++ src/main/java/TrelloManager.java
@@ -11,20 +11,16 @@ public class TrelloManager{
 
     private static final String BOARD_ID = "614de300aa6df33863299b6c";
 
-    private static final Trello trello = new TrelloImpl("e3ee0d6a1686b4b43ba5d046bbce20af","ADD YOUR TOKEN HERE");
-    private static final Board board = trello.getBoard("614de300aa6df33863299b6c");  // board (projeto)
-    private static final List<Member> members = trello.getMembersByBoard(BOARD_ID,null); // members list (if needed)
+    private static final Trello trello = new TrelloImpl("e3ee0d6a1686b4b43ba5d046bbce20af",config.MY_TOKEN);
+    private static final Board board = trello.getBoard("614de300aa6df33863299b6c");
+    private static final List<Member> members = trello.getMembersByBoard(BOARD_ID,null);
 
 
     public static void main(String[] args) {
 
     }
 
-    /**
-     * Returns the ID of a Board List provided its name.
-     * @param boardName name of the Board the user wants the ID from.
-     * @return String ID of the Board List.
-     */
+
     public static String getBoardListIdByName(String boardName){
         String listId = "";
         List<org.trello4j.model.List> boardLists = trello.getListByBoard(BOARD_ID,null);
@@ -35,7 +31,5 @@ public class TrelloManager{
         }
         return listId;
     }
-
-
 
 }
