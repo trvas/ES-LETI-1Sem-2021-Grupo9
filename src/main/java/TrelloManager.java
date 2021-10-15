1c43285d3071c6f44e4a825817e6e3dabe491b64
trvas <trvas@iscte-iul.pt>
2021-10-15 16:12:41 +0100

    backlog branch

--- src/main/java/TrelloManager.java
+++ src/main/java/TrelloManager.java
@@ -4,6 +4,8 @@ import org.trello4j.TrelloImpl;
 public class TrelloManager{
 
     public static void main(String[] args) {
+        Trello trello = new TrelloImpl("e3ee0d6a1686b4b43ba5d046bbce20af",null);
+        System.out.print(trello.getBoard("qJVls6jK").getName());
     }
 
 
