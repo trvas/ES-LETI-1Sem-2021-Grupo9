297048075b7e8e5237f5e9b7a7320c651fdf2241
Henrique-DeSousa <henriquevsousa@hotmail.com>
2021-10-20 01:56:30 +0100

    added new functionality, cleaned up some of the code, tested new functionalities

--- src/main/java/GitManager.java
+++ src/main/java/GitManager.java
@@ -1,10 +1,7 @@
+import com.google.gson.JsonArray;
 import org.kohsuke.github.*;
-
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static org.kohsuke.github.GitHub.connect;
 
@@ -12,11 +9,13 @@ import static org.kohsuke.github.GitHub.connect;
 public class GitManager extends GHAppInstallation{
 
     private static String toDelete = "henriquevsousa@hotmail.com";
-    private static String GITHUB_OAUTH = "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"; //uses github's token
-    private static String username = "Henrique-DeSousa"; //uses the user's username
+    private static String GITHUB_OAUTH = "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";//"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"; //uses github's token
+    private static String username = "trvas"; // "Henrique-DeSousa"; //uses the user's username
     private static GHUser user = new GHUser();
     private static GHRepository rep = new GHRepository();
     private static GitHub githubLogin;
+    private static ArrayList<String> finali = new ArrayList<String>();
+    private static String[] keys;
 
     private static Map<String,GHRepository> repositories = new HashMap<String, GHRepository>();
 
@@ -32,32 +31,47 @@ public class GitManager extends GHAppInstallation{
 
         }
 
-        public static void userInfo() throws IOException {
+        public static String userInfo() throws IOException {
 
-            String email = user.getBio();
             String name = user.getName();
+            long userID = user.getId();
+            String login = user.getLogin();
+            String email = user.getEmail(); if(email == null) email = "Your email isn't public";
+            String bio = user.getBio(); if(bio == null)bio   = "No bio available";
+            String location = user.getLocation(); if(location == null)location   = "Unknow location";
+            String twtUser = user.getTwitterUsername(); if(twtUser == null)twtUser = "Not available";
+            String company = user.getCompany(); if(company == null) company = "You don't have a company!";
 
-            System.out.println(email);
-            System.out.println(name);
+
+            String info = email + ";\n" + name + ";\n" + login +";\n" + bio  + ";\n" + location +";\n"+ twtUser + ";\n"+ company+ ";\n";
+            //System.out.println(info);
+            return info;
 
         }
 
         public static void repInfo() throws IOException{
-                int publicRep = user.getPublicRepoCount();
-
+                int repoCount = user.getPublicRepoCount(); if(repoCount == 0){ String noRepo = "You have no Public Repositories"; }
 
                 repositories = user.getRepositories();
-                for(Map.Entry<String, GHRepository> t : repositories.entrySet()){
-                    String a = t.getKey();
-                    String owner = t.getValue().getOwnerName();
-                    long id = t.getValue().getId();
-                    System.out.println(user.getRepository(a));
 
-                   // System.out.println("Rep Key: " + a + ". Owner: " + owner + ". ID: " + id);
+                for(Map.Entry<String, GHRepository> t : repositories.entrySet()){
+
+                    finali.add(t.getKey()); //adding Keys to the String [name]
+
+                    String owner = t.getValue().getOwnerName();
+                    Set<String> collabs = t.getValue().getCollaboratorNames();
+
+                  
+                 
+                    long id = t.getValue().getId();
+                   //System.out.println("Rep Key: " + a + ". Owner: " + owner + ". ID: " + id);
+                    System.out.println(collabs);
                 }
-            System.out.println(publicRep);
-            long id = rep.getId();
-            System.out.println(id);
+
+                String k = finali.toString();
+                String[] p = k.split(", ");
+                System.out.println(p[0]);
+                System.out.println(repoCount);
             }
 
 
@@ -66,10 +80,4 @@ public class GitManager extends GHAppInstallation{
         //String name = new GHContent().getName();
         //int total = new GHRepository().getDescription();
        // System.out.println(name + "\n");
-
-
-
-   /* public void fetch(String test){
-        GHRepository repo = GitHub.getRepository(test);
-    }*/
 }
