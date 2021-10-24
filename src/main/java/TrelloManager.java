9beb5f6cde46ed82efdf0301de8670ed3f3a7049
trvas <trvas@iscte-iul.pt>
2021-10-24 04:15:02 +0100

    changing syntax

--- src/main/java/TrelloManager.java
+++ src/main/java/TrelloManager.java
@@ -20,44 +20,27 @@ public class TrelloManager{
 
         trello = new TrelloImpl(API_KEY, TOKEN);
         Board board = trello.getBoard(BOARD_ID);
+
+
     }
 
 
     public static void main(String[] args) {
-        TrelloManager trelloManager = new TrelloManager(config.API_KEY, config.MY_TOKEN, config.BOARD_ID);
-    }
-
-
-    /**
-     * Gets the ID of cards from the Backlog pertaining to a specific Sprint. Each Increment list has
-     * a #SPRINT(NUMBER) on its label. This method iterates over the board lists until it finds one
-     * related to the sprint wanted.
-     *
-     * @param sprintNumber Sprint the user wants the cards from.
-     * @return List<Card> list of cards from the desired Sprint.
-     */
-    public static List<Card> getFinishedSprintBacklog(int sprintNumber) {
-        // Initialize auxiliary variables
-
-        // Get the list of cards from the board
-        List<Card> cards = trello.getCardsByList(getBoardListIdByName("#SPRINT" +  sprintNumber + " - Increment"));
-
-        // Returns the cards from the Done list of the sprint asked
-        return new ArrayList<>(cards);
 
     }
+
 
 
     /**
      * Returns the ID of a Board List provided its name.
-     * @param listName name of the Board the user wants the ID from.
+     * @param boardName name of the Board the user wants the ID from.
      * @return String ID of the Board List.
      */
-    public static String getBoardListIdByName(String listName){
+    public static String getBoardListIdByName(String boardName){
         String listId = "";
         List<org.trello4j.model.List> boardLists = trello.getListByBoard(BOARD_ID,null);
         for(org.trello4j.model.List boardList: boardLists) {
-            if (boardList.getName().contains(listName)) {
+            if (boardList.getName().equals(boardName)) {
                 listId = boardList.getId();
             }
         }
