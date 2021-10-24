68912d4f57a56f2afb59f9790d219afe5dff900f
Henrique-DeSousa <henriquevsousa@hotmail.com>
2021-10-24 04:17:10 +0100

    Made it simpelton proof, cleaned code and added extra functions

--- src/main/java/GitManager.java
+++ src/main/java/GitManager.java
@@ -1,4 +1,3 @@
-import com.google.gson.JsonArray;
 import org.kohsuke.github.*;
 import java.io.IOException;
 import java.util.*;
@@ -9,61 +8,81 @@ import static org.kohsuke.github.GitHub.connect;
 public class GitManager extends GHAppInstallation{
 
     private static String toDelete = "henriquevsousa@hotmail.com";
-    private static String GITHUB_OAUTH; // github's token
-    //"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" Tati // "trvas"
-    //"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"; Henrique // "Henrique-DeSousa"
-    private static String username;  //uses the user's username
+    private static String GITHUB_OAUTH; // gitHub's token
+    private static String GITHUB_LOGIN;  //uses the user's username
+    //"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" Tatiana // "trvas"
+    //"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"; Henrique // "Henrique-DeSousa"
+
     private static GHUser user = new GHUser();
-    private static GHRepository rep = new GHRepository();
     private static GitHub githubLogin;
     private static ArrayList<String> finali = new ArrayList<String>();
-    private static String[] keys;
+    private static boolean valid = false;
 
     private static Map<String,GHRepository> repositories = new HashMap<String, GHRepository>();
 
     public static void main(String[] args) throws IOException {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username: ");
-
-        username = myObj.nextLine();  // Read user input
-        System.out.println("Enter Token: ");
-        GITHUB_OAUTH = myObj.nextLine(); // read token
-        System.out.println("Username is: " + username + "\nToken is: " + GITHUB_OAUTH);  // Output user input
-
-
-        connect();
+        login();
         userInfo();
         repInfo();
     }
-        public static void connect() throws IOException {
-            githubLogin = new GitHubBuilder().withOAuthToken(GITHUB_OAUTH, username).build().connectUsingOAuth(GITHUB_OAUTH);
-            user = githubLogin.getUser(username);
-            //System.out.println(githubLogin.getMyself());
 
+        public static void login() throws IOException { // login for the user
+            Scanner scan = new Scanner(System.in);  // Create a Scanner object
+
+            while (!valid){
+                System.out.println("Enter username: ");
+                String useTemp = scan.nextLine();  // Read user input
+
+                System.out.println("Enter Token: ");
+                String authTemp = scan.nextLine(); // read token
+                new GitManager(authTemp, useTemp);
+                connect();
+            }//System.out.println("Username is: " + GITHUB_LOGIN + "\nToken is: " + GITHUB_OAUTH);  // Output user input
+        }
+
+        public GitManager(String ID, String userName){
+            this.GITHUB_LOGIN = userName;
+            this.GITHUB_OAUTH = ID;
+        }
+
+        public static void connect() throws IOException {
+            githubLogin = new GitHubBuilder().withOAuthToken(GITHUB_OAUTH, GITHUB_LOGIN).build();
+                if(githubLogin.isCredentialValid()) {
+                    valid = true;
+                    githubLogin.connect(GITHUB_LOGIN, GITHUB_OAUTH);
+                    user = githubLogin.getUser(GITHUB_LOGIN);
+                }
         }
 
         public static String userInfo() throws IOException {
 
             String name = user.getName();
             long userID = user.getId();
+
+            String token = user.getAvatarUrl(); // add this to the UI
+            System.out.println(token);
+
             String login = user.getLogin();
             String email = user.getEmail(); if(email == null) email = "Your email isn't public";
             String bio = user.getBio(); if(bio == null)bio   = "No bio available";
-            String location = user.getLocation(); if(location == null)location   = "Unknow location";
+            String location = user.getLocation(); if(location == null)location   = "Unknown location";
             String twtUser = user.getTwitterUsername(); if(twtUser == null)twtUser = "Not available";
             String company = user.getCompany(); if(company == null) company = "You don't have a company!";
 
 
-            String info = email + ";\n" + name + ";\n" + login +";\n" + bio  + ";\n" + location +";\n"+ twtUser + ";\n"+ company+ ";\n";
-            //System.out.println(info);
+            String info = "\n" + email + ";\n" + name + ";\n" + login +";\n" + bio  + ";\n" + location +";\n"+ twtUser + ";\n"+ company + ".\n";
+            System.out.println(info);
             return info;
-
         }
 
         public static void repInfo() throws IOException{
                 int repoCount = user.getPublicRepoCount(); if(repoCount == 0){ String noRepo = "You have no Public Repositories"; }
+                Optional<Integer> privRepoCount = user.getTotalPrivateRepoCount(); // needs proper token to find the private
+
+                //System.out.println("Number of Private Repositories: " + privRepoCount.get());
 
                 repositories = user.getRepositories();
+                System.out.println(repoCount);
 
                 for(Map.Entry<String, GHRepository> t : repositories.entrySet()){
 
@@ -81,14 +100,8 @@ public class GitManager extends GHAppInstallation{
 
                 String k = finali.toString();
                 String[] p = k.split(", ");
-                System.out.println(p[0]);
-                System.out.println(repoCount);
+                System.out.println(p[1]);
+
+
             }
-
-
-
-
-        //String name = new GHContent().getName();
-        //int total = new GHRepository().getDescription();
-       // System.out.println(name + "\n");
 }
---
+++ src/main/java/GitManagerTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GitManagerTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void connect() {
+    }
+
+    @Test
+    void userInfo() {
+    }
+
+    @Test
+    void repInfo() {
+    }
+}
