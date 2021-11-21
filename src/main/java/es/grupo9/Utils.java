a2e9e7b02d1b8a4ef462cec4e5eb7d7d8a94bcab
pjspa-iscte <pjspa@iscte-iul.pt>
2021-11-21 18:00:29 +0000

    adding get cost

--- src/main/java/es/grupo9/Utils.java
+++ src/main/java/es/grupo9/Utils.java
@@ -3,6 +3,27 @@ package es.grupo9;
 public class Utils{
 
 
+    public static int getCusto(int horas, int custo) {
+        return horas * custo;
+    }
+
+    // custo default = 20
+    public static int getCusto(int horas) {
+        getCusto(horas, 20);
+    }
+
+    @Override
+    public String toString() {
+        return "CalculatingCost [horas=" + horas + ", custo=" + custo + "]";
+    }
+
+
+
+
+
+
+
+
     public static void utils(String[] args) {
     }
 }
