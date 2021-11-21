ad419548b7be9c18dc52504dc69889fae5d48339
trvas <trvas@iscte-iul.pt>
2021-11-21 17:28:52 +0000

    re-adding trelloManager classes + tests.

--- src/main/java/com/example/demo/TrelloManager.java
+++ src/main/java/com/example/demo/TrelloManager.java
@@ -4,38 +4,90 @@ import org.trello4j.Trello;
 import org.trello4j.TrelloImpl;
 import org.trello4j.model.Board;
 import org.trello4j.model.Card;
-import org.trello4j.model.Member;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 public class TrelloManager{
 
-    private static final String BOARD_ID = "614de300aa6df33863299b6c";
+    private static Trello trello;
+//    private static Board board;
 
-    private static final Trello trello = new TrelloImpl("e3ee0d6a1686b4b43ba5d046bbce20af",null);
-    private static final Board board = trello.getBoard("614de300aa6df33863299b6c");
-    private static final List<Member> members = trello.getMembersByBoard(BOARD_ID,null);
+    private static String BOARD_ID;
+
+    public TrelloManager(String API_KEY, String TOKEN, String BOARD_ID) {
+
+        this.BOARD_ID = BOARD_ID;
+
+        trello = new TrelloImpl(API_KEY, TOKEN);
+        Board board = trello.getBoard(BOARD_ID);
+    }
 
 
     public static void main(String[] args) {
+    //    TrelloManager trelloManager = new TrelloManager(config.API_KEY, config.MY_TOKEN, config.BOARD_ID);
+    }
 
+
+    /**
+     * Gets the ID of cards from the Backlog pertaining to a specific Sprint. Each Increment list has
+     * a #SPRINT(NUMBER) on its label. This method iterates over the board lists until it finds one
+     * related to the sprint wanted.
+     *
+     * @param sprintNumber Sprint the user wants the cards from.
+     * @return List<Card> list of cards from the desired Sprint.
+     */
+    public static List<Card> getFinishedSprintBacklog(int sprintNumber) {
+        // Initialize auxiliary variables
+
+        // Get the list of cards from the board
+        List<Card> cards = trello.getCardsByList(getBoardListIdByName("#SPRINT" +  sprintNumber + " - Increment"));
+
+        // Returns the cards from the Done list of the sprint asked
+        return new ArrayList<>(cards);
     }
 
     /**
      * Returns the ID of a Board List provided its name.
-     * @param boardName name of the Board the user wants the ID from.
+     * @param listName name of the Board the user wants the ID from.
      * @return String ID of the Board List.
      */
-    public static String getBoardListIdByName(String boardName){
+    public static String getBoardListIdByName(String listName){
         String listId = "";
         List<org.trello4j.model.List> boardLists = trello.getListByBoard(BOARD_ID,null);
         for(org.trello4j.model.List boardList: boardLists) {
-            if (boardList.getName().equals(boardName)) {
+            if (boardList.getName().contains(listName)) {
                 listId = boardList.getId();
             }
         }
         return listId;
+    }
+
+    /**
+     * Returns a HashMap with all the cards of the meetings in each Sprint.
+     * @return HashMap<Integer, List<Card>> HashMap with the Sprint Number and list of cards (meetings) from that sprint.
+     */
+    public static HashMap<Integer, List<Card>> getMeetings(){
+        HashMap<Integer, List<Card>> meetings = new HashMap<>();
+
+        // Get all the Meetings cards
+        for(int sprintNumber = 1; sprintNumber < getSprintCount() + 1; sprintNumber++){
+            meetings.put(sprintNumber, trello.getCardsByList(getBoardListIdByName("#SPRINT" + sprintNumber + " - Meetings")));
+        }
+
+        return meetings;
+    }
+
+    /**
+     * Gets the number of current Sprints.
+     * @return int number of Sprints so far.
+     */
+    public static int getSprintCount(){
+        // Get "Sprints" list cards
+        List<Card> cards = new ArrayList<>(trello.getCardsByList(getBoardListIdByName("Sprints")));
+
+        return cards.size();
     }
 
 }
--- src/test/java/TrelloManagerTest.java
+++ src/test/java/TrelloManagerTest.java
@@ -1,28 +1,43 @@
+import com.example.demo.TrelloManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import com.example.demo.TrelloManager;
 
 class TrelloManagerTest {
 
-    /*
+    TrelloManager trelloManager;
+    final String BOARD_ID = "614de300aa6df33863299b6c"; // ID of the board we are currently using
+
     @BeforeEach
     void setUp() {
-        TrelloManager trelloManager = new TrelloManager("e3ee0d6a1686b4b43ba5d046bbce20af", config.MY_TOKEN, "614de300aa6df33863299b6c");
-
+        TrelloManager trelloManager = new TrelloManager("e3ee0d6a1686b4b43ba5d046bbce20af", config.MY_TOKEN, BOARD_ID);
     }
-    */
-
 
     @AfterEach
     void tearDown() {
     }
 
-    // add tests
     @Test
     void getBoardListIdByName() {
+        // ID Lista "Sprints" da Board = 61606295191d043999a57bcb
+        Assertions.assertEquals("61606295191d043999a57bcb", TrelloManager.getBoardListIdByName("Sprints"));
+    }
 
+    @Test
+    void getFinishedSprintBacklog() {
+        Assertions.assertNotEquals(null,TrelloManager.getFinishedSprintBacklog(1));
+    }
 
+    @Test
+    void getMeetings() {
+        // ID to primeiro cartão da lista Meetings = 616485eb23537a5ed11aec71
+        Assertions.assertEquals(TrelloManager.getMeetings().get(1).get(0).getId(),"616485eb23537a5ed11aec71");
+    }
 
+    @Test
+    void getSprintCount() {
+        // Número de SPRINTS = 3
+        Assertions.assertEquals(TrelloManager.getSprintCount(),3);
     }
 }
